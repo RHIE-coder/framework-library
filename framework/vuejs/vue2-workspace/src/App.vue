49e58ce9b522db024845da155977c8abb3753e9f
rhie-coder <quotia72@naver.com>
2022-10-20 17:13:35 +0900

    STUDY vue:project init ongoing

---
+++ framework/vuejs/vue2-workspace/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <h1>HELLO WORLD</h1>
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Application",
+    data: function(){
+        return {
+            message: "Hello World",
+        }
+    }
+}
+</script>
+
