d6d2661c338a08ac0dec297e5d49731726e7bade
root <root@DESKTOP-MGQLLUU>
2023-02-02 18:08:25 +0900

    STUDY(golang)

--- golang/lab/src/hello/hello.go
+++ golang/lab/src/hello/hello.go
@@ -2,14 +2,29 @@ package main
 
 import "fmt"
 
-func main() {
-
-	var total int
-
-	result := func(nums ...int) {
-		for _, num := range nums {
-			total += num
-		}
-		fmt.Println(total)
-	}()
+type Rectangle struct {
+	width  int
+	height int
 }
+
+func scaleOut(rect *Rectangle) {
+	rect.width *= 10
+	rect.height *= 10
+}
+func main() {
+	rect := Rectangle{2, 3}
+	rectPtr := &Rectangle{2, 3}
+	fmt.Println(rect)
+	fmt.Println(rectPtr)
+	scaleOut(&rect)
+	scaleOut(rectPtr)
+	fmt.Println(rect)
+	fmt.Println(rectPtr)
+}
+
+/*
+{2 3}
+&{2 3}
+{20 30}
+&{20 30}
+*/
