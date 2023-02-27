5748e2ef9c52435fcebc050529a4b469fe05290d
rhie-coder <quotia72@naver.com>
2023-02-27 17:46:55 +0900

    STUDY(nodejs)

--- nodejs/sample/cryp.js
+++ nodejs/sample/cryp.js
@@ -1,2 +1,13 @@
 const bcrypt = require("bcryptjs")
 
+const inputPassword = "gr!";
+
+
+(async() => {
+    const salt = bcrypt.genSaltSync(10);
+    console.log(salt);
+    const storedPassword = await bcrypt.hash(inputPassword, salt);
+    console.log(storedPassword)
+    const matchResult = bcrypt.compareSync(inputPassword, storedPassword)
+    console.log(matchResult);
+})()
